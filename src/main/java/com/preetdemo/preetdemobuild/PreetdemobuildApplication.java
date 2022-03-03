3b96375c8a14b7c6ca0653f28cd7438bf88d9f0e
Gurpreetw <preetgur.gsr@gmail.com>
2022-03-03 18:07:10 +0530

    demo code

---
+++ src/main/java/com/preetdemo/preetdemobuild/PreetdemobuildApplication.java
@@ -0,0 +1,13 @@
+package com.preetdemo.preetdemobuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreetdemobuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreetdemobuildApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/preetdemo/preetdemobuild/PreetdemobuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.preetdemo.preetdemobuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreetdemobuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
